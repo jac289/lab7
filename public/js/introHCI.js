b398fcebd6f5edb413e8d8c66537228e2ca0dd28
Jaclyn Chin <jac289@ucsd.edu>
2019-02-21 10:26:28 -0800

    hi

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,12 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(likeClicked);
+}
+
+function likeClicked(e){
+	e.preventDefault();
+	console.log("clickin");
+	ga('create', 'UA-134865038-1', 'auto');
+	ga("send", "event", "like", "click");
 }
